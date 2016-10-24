38aee9eb763f609fe95135d2e7959d6d3a382416
skisonic <skisonic@gmail.com>
2016-10-24 07:03:10 -0400

    bullets

---
+++ Assets/scripts/FireCannonOnGaze.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using Tobii.EyeTracking;
+
+[RequireComponent(typeof(GazeAware))]
+public class FireCannonOnGaze : MonoBehaviour
+{
+    private GazeAware _gazeAware;
+    public GameObject bullet_pf;
+    GameObject cannon;
+    GazePoint gazePoint;
+
+    GameObject bullet;
+
+    void Start()
+    {
+        _gazeAware = GetComponent<GazeAware>();
+        cannon = GameObject.Find("CannonL");
+    }
+
+    void Update()
+    {
+        if (_gazeAware.HasGazeFocus || Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            bullet = Instantiate(bullet_pf, transform.position, Quaternion.Euler(new Vector3(0,0,90f + transform.localRotation.z))) as GameObject;
+            //bullet.transform.parent = gameObject.transform;
+            bullet.GetComponent<Rigidbody>().AddForce(Vector3.right * 1000);
+            //bullet.transform.parent = null;
+            //gazePoint = EyeTracking.GetGazePoint();
+            //Debug.Log("found it");
+        }
+    }
+}
---
+++ Assets/scripts/RotateCannon.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine; 
+using System.Collections;
+
+public class RotateCannon : MonoBehaviour {
+
+
+    public GameObject sphereL, sphereR;
+
+    bool isShrinkingL, isShrinkingR;
+    float shrinkRange = 100.0f;
+    float shrinkFactorL = 100.0f;
+    float shrinkFactorR = 100.0f;
+    float shrinkRateL = 3.0f;
+    float shrinkRateR = 1.5f;
+    // Use this for initialization
+    void Start () {
+        isShrinkingL = true;
+        isShrinkingR = true;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate () {
+
+        if (isShrinkingL)
+        {
+            sphereL.transform.Rotate(Vector3.forward, -shrinkRateL);
+            shrinkFactorL = shrinkFactorL - shrinkRateL;
+        }
+        else if (!isShrinkingL)
+        {
+            sphereL.transform.Rotate(Vector3.forward, shrinkRateL);
+            shrinkFactorL = shrinkFactorL + shrinkRateL;
+        }
+
+
+        if (isShrinkingR)
+        {
+            sphereR.transform.Rotate(Vector3.forward, -shrinkRateR);
+            shrinkFactorR = shrinkFactorR - shrinkRateR;
+        }
+        else if (!isShrinkingR)
+        {
+            sphereR.transform.Rotate(Vector3.forward, shrinkRateR);
+            shrinkFactorR = shrinkFactorR + shrinkRateR;
+        }
+
+        if (shrinkFactorL >= shrinkRange)
+        {
+            isShrinkingL = true;
+        }
+        else if (shrinkFactorL <= 0)
+        {
+            isShrinkingL = false;
+        }
+
+        if (shrinkFactorR >= shrinkRange)
+        {
+            isShrinkingR = true;
+        }
+        else if (shrinkFactorR <= 0)
+        {
+            isShrinkingR = false;
+        }
+    }
+}
+    
